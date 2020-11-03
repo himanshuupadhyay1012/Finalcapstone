b9b0d75cd7f7edce5e5c02a469ef9ddb8f0a338a
himanshuupadhyay1012 <73900797+himanshuupadhyay1012@users.noreply.github.com>
2020-11-03 22:50:31 +0530

    Update StudentsApplication.java

--- src/main/java/net/techtter/demo/student/StudentsApplication.java
+++ src/main/java/net/techtter/demo/student/StudentsApplication.java
@@ -3,14 +3,14 @@ package net.techtter.demo.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
- server.error.whitelabel.enabled=false
- @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+// server.error.whitelabel.enabled=false
+ //@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @SpringBootApplication
 public class StudentsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentsApplication.class, args);
 	}
-@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 
 }
